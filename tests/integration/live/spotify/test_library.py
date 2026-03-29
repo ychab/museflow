074dab409ba308f03d098d11bbb905ce43d71f4b
Yannick Chabbert <yannick.chabbert.it@gmail.com>
2026-03-29 12:27:13 +0200

    Improve missing live test on spotify

--- tests/integration/live/spotify/test_library.py
+++ tests/integration/live/spotify/test_library.py
@@ -112,11 +112,26 @@ class TestSpotifyLibraryLive:
 
     async def test_playlist_tracks(self, spotify_library_live: SpotifyLibraryAdapter) -> None:
         playlist_tracks = await spotify_library_live.get_playlist_tracks(page_size=2, max_pages=1)
-        assert len(playlist_tracks) == 2 * (2 * 1)
+        assert len(playlist_tracks) > 0
 
-    async def test_search(self, spotify_library: SpotifyLibraryAdapter) -> None:
-        tracks = await spotify_library.search_tracks(track="Mi Pueblo", page_size=5, max_pages=1)
-        assert len(tracks) == 5
+    async def test_get_track_by_id(self, spotify_library_live: SpotifyLibraryAdapter, tracks: list[Track]) -> None:
+        track = await spotify_library_live.get_track_by_id(tracks[0].provider_id)
+
+        assert track.provider_id == tracks[0].provider_id
+        assert track.name
+        assert len(track.artists) > 0
+
+    async def test_get_tracks_by_ids(self, spotify_library_live: SpotifyLibraryAdapter, tracks: list[Track]) -> None:
+        ids = [t.provider_id for t in tracks]
+
+        result = await spotify_library_live.get_tracks_by_ids(ids)
+
+        assert len(result) == len(ids)
+        assert all(t.provider_id in ids for t in result)
+
+    async def test_search(self, spotify_library_live: SpotifyLibraryAdapter) -> None:
+        tracks = await spotify_library_live.search_tracks(track="Mi Pueblo", page_size=5, max_pages=1)
+        assert len(tracks) > 0
 
     async def test_playlist_create(
         self,
