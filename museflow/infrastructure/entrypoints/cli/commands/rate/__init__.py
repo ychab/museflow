f6f4bf85c7d80a2da0ee7469b829ba378a71c1dc
Yannick Chabbert <yannick.chabbert.it@gmail.com>
2026-06-16 15:41:10 +0200

    Remove rate track subcommand

--- museflow/infrastructure/entrypoints/cli/commands/rate/__init__.py
+++ museflow/infrastructure/entrypoints/cli/commands/rate/__init__.py
@@ -4,4 +4,3 @@ app = typer.Typer()
 
 import museflow.infrastructure.entrypoints.cli.commands.rate.history  # noqa: F401,E402
 import museflow.infrastructure.entrypoints.cli.commands.rate.playlist  # noqa: F401,E402
-import museflow.infrastructure.entrypoints.cli.commands.rate.track  # noqa: F401,E402
